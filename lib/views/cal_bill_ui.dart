8b47f38a1eefa4d2e5135e482da96c5686e0e269
GRG02 <ratipong818@gmail.com>
2025-03-19 11:11:19 +0700

    ย้อนกลับได้

--- lib/views/cal_bill_ui.dart
+++ lib/views/cal_bill_ui.dart
@@ -469,7 +469,21 @@ class _CallBillUIState extends State<CallBillUI> {
                     ),
                     Expanded(
                       child: ElevatedButton.icon(
-                        onPressed: () {},
+                        onPressed: () {
+                          //ทุกอย่างบนหน้าจอกลับเป็นค่าเริ่มต้นหรือเหมือนเดิม
+                          setState(() {
+                            imgFile = null;
+                            isAdult = false;
+                            isChild = false;
+                            isWater = false;
+                            adultCtrl.clear();
+                            childCtrl.clear();
+                            cokeCtrl.clear();
+                            pureCtrl.clear();
+                            groupWater = 1;
+                            _selectedMember = 'ไม่เป็นสมาชิก';
+                          });
+                        },
                         icon: Icon(
                           Icons.cancel,
                           color: Colors.white,
