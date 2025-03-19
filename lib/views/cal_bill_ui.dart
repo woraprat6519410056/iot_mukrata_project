f2637065e30f0e8d3911b5344a1bb93343dee29d
GRG02 <ratipong818@gmail.com>
2025-03-19 10:57:39 +0700

    เหลือกดย้อน ใบเสร็จเสร็จเเล้ว

--- lib/views/cal_bill_ui.dart
+++ lib/views/cal_bill_ui.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:iot_mukrata_project/views/show_bill_ui.dart';
 
 class CallBillUI extends StatefulWidget {
   const CallBillUI({super.key});
@@ -54,6 +55,30 @@ class _CallBillUIState extends State<CallBillUI> {
     setState(() {
       imgFile = File(image.path);
     });
+  }
+
+  //สร้างเมธอดแสดงข้อความเตือน
+  Future<void> showWarningDialog(context, msg) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('คำเตือน'),
+          content: Text(
+            msg,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('ตกลง'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -228,6 +253,7 @@ class _CallBillUIState extends State<CallBillUI> {
                           pureCtrl.clear();
                         });
                       },
+                      activeColor: Colors.deepOrange,
                     ),
                     Text(
                       'รับ 25 บาท/หัว',
@@ -245,6 +271,7 @@ class _CallBillUIState extends State<CallBillUI> {
                           isWater = true;
                         });
                       },
+                      activeColor: Colors.deepOrange,
                     ),
                     Text(
                       'ไม่รับ',
@@ -345,13 +372,76 @@ class _CallBillUIState extends State<CallBillUI> {
                   ),
                 ),
                 SizedBox(
-                  height: 20.0,
+                  height: 30.0,
                 ),
                 Row(
                   children: [
                     Expanded(
                       child: ElevatedButton.icon(
-                        onPressed: () {},
+                        onPressed: () {
+                          //Validate UI
+                          if (isAdult == true && adultCtrl.text.isEmpty ||
+                              adultCtrl.text == '0') {
+                            showWarningDialog(
+                                context, 'กรุณากรอกจํานวนผู้ใหญ่ด้วย');
+                          } else if (isChild == true &&
+                                  childCtrl.text.isEmpty ||
+                              childCtrl.text == '0') {
+                            showWarningDialog(
+                                context, 'กรุณากรอกจํานวนเด็กด้วย');
+                          } else {
+                            //คำนวนเงิน
+                            //เตรียมข้อมูลที่ต้องใช้เพื่อการคำนวณ
+                            int numAdult =
+                                isAdult == true ? int.parse(adultCtrl.text) : 0;
+                            int numChild =
+                                isChild == true ? int.parse(childCtrl.text) : 0;
+                            int numCoke = cokeCtrl.text.isEmpty
+                                ? 0
+                                : int.parse(cokeCtrl.text);
+                            int numPure = pureCtrl.text.isEmpty
+                                ? 0
+                                : int.parse(pureCtrl.text);
+                            double sale = 0.0;
+                            if (_selectedMember == 'สมาชิกทั่วไปลด 10%') {
+                              sale = 0.1;
+                            } else if (_selectedMember == 'สมาชิก VIP ลด 20%') {
+                              sale = 0.2;
+                            }
+                            double payWaterBuffet = groupWater == 1
+                                ? 25.0 * (numAdult + numChild)
+                                : 0.0;
+                            //คำนวณยังไม่ได้คิดส่วนลด
+                            double payBuffetTotalNoSale = (numAdult * 299.0) +
+                                (numChild * 69.0) +
+                                (numCoke * 20.0) +
+                                (numPure * 15.0) +
+                                payWaterBuffet;
+                            //คำนวณส่วนลด
+                            double paySale = payBuffetTotalNoSale * sale;
+                            //คำนวณที่ต้องจ่ายหลังหักส่วนลดแล้ว
+                            double payBuffetTotal =
+                                payBuffetTotalNoSale - paySale;
+
+                            //ส่งค่าต่างๆ ไปแสดงที่หน้า ShowBillUI()
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ShowBillUi(
+                                  numAdult: numAdult,
+                                  numChild: numChild,
+                                  numCoke: numCoke,
+                                  numPure: numPure,
+                                  payWaterBuffet: payWaterBuffet,
+                                  payBuffetTotalNoSale: payBuffetTotalNoSale,
+                                  paySale: paySale,
+                                  payBuffetTotal: payBuffetTotal,
+                                  imageFile: File,
+                                ),
+                              ),
+                            );
+                          }
+                        },
                         icon: Icon(
                           Icons.calculate,
                           color: Colors.white,
@@ -364,11 +454,18 @@ class _CallBillUIState extends State<CallBillUI> {
                         ),
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.green,
+                          fixedSize: Size(
+                            double.infinity,
+                            60.0,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
                       ),
                     ),
                     SizedBox(
-                      width: 20.0,
+                      width: 15.0,
                     ),
                     Expanded(
                       child: ElevatedButton.icon(
@@ -385,6 +482,13 @@ class _CallBillUIState extends State<CallBillUI> {
                         ),
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.red,
+                          fixedSize: Size(
+                            double.infinity,
+                            60.0,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
                       ),
                     ),
--- lib/views/show_bill_ui.dart
+++ lib/views/show_bill_ui.dart
@@ -1,7 +1,34 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class ShowBillUi extends StatefulWidget {
-  const ShowBillUi({super.key});
+  //สร้างตัวแปรเพื่อรับข้อมูลที่จะส่งมาจากหน้า CalBillUI()
+  int? numAdult;
+  int? numChild;
+  int? numCoke;
+  int? numPure;
+  double? payWaterBuffet;
+  double? payBuffetTotalNoSale;
+  double? paySale;
+  double? payBuffetTotal;
+  File? imageFile;
+
+  //เอาตัวแปลทีที่ารา้งไปรับข้อมูลที่ส่งมา
+  ShowBillUi({
+    super.key,
+    this.numAdult,
+    this.numChild,
+    this.numCoke,
+    this.numPure,
+    this.payWaterBuffet,
+    this.payBuffetTotalNoSale,
+    this.paySale,
+    this.payBuffetTotal,
+    required Type imageFile,
+  });
+
+  get numCild => null;
 
   @override
   State<ShowBillUi> createState() => _ShowBillUiState();
@@ -10,6 +37,116 @@ class ShowBillUi extends StatefulWidget {
 class _ShowBillUiState extends State<ShowBillUi> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+          'ใบเสร็จรับเงิน',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              widget.imageFile == null
+                  ? Image.asset(
+                      'assets/images/camera.jpg',
+                      width: 150,
+                      height: 150,
+                    )
+                  : Image.file(
+                      widget.imageFile!,
+                      width: 200,
+                      height: 200,
+                    ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                'รายละเอียดใบเสร็จรับเงิน',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'จำนวนผู้ใหญ่ ${widget.numAdult} คน',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'จำนวนผู้เด็ก ${widget.numChild} คน',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'จำนวนโค้ก ${widget.numCoke} ขวด',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'จำนวนน้ำเปล่า ${widget.numPure} ขวด',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'จำนวนเงินบุฟเฟต์น้ำ ${widget.payWaterBuffet} บาท',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'ส่วนลดเป็นเงิน ${widget.paySale} บาท',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'รวมต้องจ่าย ${widget.payBuffetTotal} บาท',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Image.asset(
+                'assets/images/payqr.png',
+                width: 250,
+                height: 250,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
